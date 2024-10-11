7e0d46c9f381f4b010ffc5edb1420a6d820344c1
ym1225 <zeromin1673@gmail.com>
2024-10-11 16:06:49 +0900

    두번째 commit

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello,world!");		
+		System.out.println("Hello,world!");	
+        System.out.println("2Hello,world!");		
 	}
 
 }
